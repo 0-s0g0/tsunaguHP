85b51855feb3f7bdf8cc7dce562803eb4485b8ee
0-s0g0 <sogo.rei812@gmail.com>
2025-05-18 15:14:37 +0900

    ini

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,12 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import Header from "@/components/header"
-import Footer from "@/components/footer"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "有限会社北九電機",
-  description: "配電盤の製造、修理及び工事施工、電気設備工事など幅広いサービスを提供しています。",
+  title: "タイトル",
+  description: "詳細",
 }
 
 export default function RootLayout({
@@ -21,9 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
           <div className="flex  flex-col  justify-center ">
-            <Header />
             <main className="flex-1">{children}</main>
-            <Footer />
           </div>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,7 @@
-import Link from "next/link"
-import Image from "next/image"
-import { Button } from "@/components/ui/button"
-import { Card, CardContent } from "@/components/ui/card"
-import { ArrowRight, Zap, Shield, PenToolIcon as Tool, Settings, Users, Phone } from "lucide-react"
-import HomeMain from "./home/components/main"
-import ServiceHome from "./home/components/services"
-import CompanyHome from "./home/components/company"
-import CareerHome from "./home/components/careers"
-
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <HomeMain/>
-
-      <ServiceHome/>
-
-      <CompanyHome/>
-
-      <CareerHome/>
-
-      
-    </div>
+<>
+nextapp
+</>
   )
 }
